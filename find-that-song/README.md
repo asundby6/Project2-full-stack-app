7a97e4757fc8bccf3aa83fdd9fa13d9a0c6782ef
asundby6 <asundby6@yahoo.com>
2023-01-27 14:36:25 -0500

    Fixed boiler plate, decide which api asap

---
+++ find-that-song/README.md
@@ -0,0 +1,21 @@
+Overview
+
+The Find That Song app is a useful search engine for songs that you can only manage to remember however many lyrics from. Maybe it's just a few words from the hook, or maybe it's an entire line of lyrics. Either way my song finder will be able to connect you to that fragmented memory. The search enginge works in relationship to how many details you can provide the application with.
+
+User Stories
+
+Users will be able to experience the following : 
+- Register an account 
+- Login to an account 
+- Logout of account 
+- User searched page that only displays when an account is made 
+- Search the lyrics to get the song name 
+- Search by the artist, if it's known 
+- add to a list of successful searches for a display to all users
+
+Wireframes / Screenshots
+
+
+
+
+Entity Relationship Diagrams
---
+++ find-that-song/server.js
@@ -0,0 +1,85 @@
+////////////////////
+//  Dependencies  //
+////////////////////
+require("dotenv").config() // make env variables available
+const express = require("express")
+const middleware = require('./utils/middleware')
+const ExampleRouter = require('./controllers/example')
+const UserRouter = require('./controllers/user')
+const User = require("./models/user")
+// SEE MORE DEPENDENCIES IN ./utils/middleware.js
+// user and resource routes linked in ./utils/middleware.js
+
+//////////////////////////////
+// Middleware + App Object  //
+//////////////////////////////
+const app = require("liquid-express-views")(express())
+
+middleware(app)
+
+////////////////////
+//    Routes      //
+////////////////////
+
+app.use('/auth', UserRouter)
+app.use('/examples', ExampleRouter)
+
+app.get('/', (req, res) => {
+    const { username, userId, loggedIn } = req.session
+	res.render('index.liquid', { loggedIn, username, userId })
+})
+
+app.get('/error', (req, res) => {
+	const error = req.query.error || 'This Page Does Not Exist'
+    const { username, loggedIn, userId } = req.session
+	res.render('error.liquid', { error, username, loggedIn, userId })
+})
+
+// if page is not found, send to error page
+app.all('*', (req, res) => {
+	res.redirect('/error')
+})
+
+
+
+//////////////////////////////
+//      App Listener        //
+//////////////////////////////
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
+})
+
+
+
+
+
+/*
+user = {
+	username: { type: String },
+	password: { type: String }
+}
+savedSongs = {
+	owner: userId ref, // just like fruits,
+	title: string,
+	artist: string,
+	song_id: string, // probably going to want lyricId from chartAPI
+	lyrics: String
+}
+songLists = {
+	owner: userId ref, // just like fruits,
+	name: String,
+	songs: [{ type: objectId, ref: 'savedSongs' }]
+}
+------------------------------------------------------------------------------------------------------------------------------
+- Proj name -
+Find That Song
+- Idea + Notes -
+Users can CRUD song lyrics
+- API -
+http://www.chartlyrics.com/api.aspx
+- MVP -
+Users can search songs by lyrics, and/or artist. Save songs they've found. Users can use those saved songs to create lists. 
+- Stretch Goals -
+figure out pagination(limit # of results shown on a single page)
+song ratings(an additional model that connects users to saved songs, allows them to rate from 1-5)
+*/
